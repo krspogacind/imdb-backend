ae756f3a5d45c62c448d419f3760f1d217ad8b95
Darko Krspogacin <darko.krspogacin@vivifyideas.com>
2020-12-25 11:21:26 +0100

    create movies endpoint

---
+++ imdb-backend-app/database/migrations/2020_12_25_082004_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Enumerations\GenreEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('cover_image_url');
+            $table->unsignedBigInteger('genre_id');
+            $table->timestamps();
+
+            $table->foreign('genre_id')->references('id')->on('genres');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
